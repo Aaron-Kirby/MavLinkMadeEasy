2ae80b6b0335cd9a623bed23152a973f748b9689
kt-sin <katie.sindelar@gmail.com>
2018-06-25 22:50:06 +0000

    updated admin for new models

--- mavAgenda/landing/admin.py
+++ mavAgenda/landing/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Question
+from .models import User, DegreePath, Course
 # Register your models here.
 
-admin.site.register(Question)
+admin.site.register(User)
+admin.site.register(DegreePath)
+admin.site.register(Course)
+
