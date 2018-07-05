fd1cb043c8eb28ed18cd21bb0cbca16190841f04
Lauren Koperski <laurenkoperski@gmail.com>
2018-07-04 23:02:05 -0500

    Attempted form creation

--- mavAgenda/landing/urls.py
+++ mavAgenda/landing/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'landing'
 urlpatterns = [
     path('', views.login, name='login'),
-    path('selectdegree/', views.selectdegree, name='selectdegree'),
+    #path('selectdegree/', views.selectdegree, name='selectdegree'),
+    path('selectcourses/', views.selectcourses, name='selectcourses'),
+    path('schedule/', views.schedule, name='schedule'),
+    path('createuser/', views.createuser, name='createuser'),
 ]
--- mavAgenda/landing/views.py
+++ mavAgenda/landing/views.py
@@ -7,14 +7,39 @@ from django.urls import reverse
 from .models import User
 from .models import DegreePath
 from .models import Courses
+from .forms import LoginForm
 
-def selectdegree(request):
-    return render(request, 'landing/selectdegree.html')
+#def selectdegree(request):
+    #return render(request, 'landing/selectdegree.html')
 
 def login(request):
-    return render(request, 'landing/login.html')
-    
-    
+#def get_email(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/schedule/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = LoginForm()
+
+    return render(request, '/landing/login.html', {'form': form})
+
+def selectcourses(request):
+    return render(request, 'landing/selectcourses.html')
+
+def schedule(request):
+    return render(request, 'landing/schedule.html')
+
+def createuser(request):
+    return render(request, 'landing/createuser.html')
+
 
 
 
