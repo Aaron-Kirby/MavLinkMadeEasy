066b8a8f1162c975c66e9ce8e8705f6a54ccda67
kt-sin <katie.sindelar@gmail.com>
2018-07-12 00:52:26 +0000

    minor modifications to improve admin display

--- mavAgenda/landing/admin.py
+++ mavAgenda/landing/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
-from .models import Course, Degree, ReqType, Prereq
-from .models import User, UserCompleted
+from .models import Course, Degree, Req, Prereq, User, Complete
 
 # Register models below
 
 admin.site.register(User)
-admin.site.register(UserCompleted)
+admin.site.register(Complete)
 admin.site.register(Course)
 # admin.site.register(Degree)
 admin.site.register(Prereq)
-admin.site.register(ReqType)
+admin.site.register(Req)
+
 
 class DegreeAdmin(admin.ModelAdmin):
-    list_display = ('degree', 'major', 'concentration')
-    list_filter = ('level', 'major')
-    fields = ['degree', ('major', 'concentration')]
+    list_display = ('degree', 'major')
+    list_filter = ('degree', 'major')
+    fields = ['degree', 'major', 'req']
 
 admin.site.register(Degree, DegreeAdmin)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('num', 'name', 'special', 'credits')
+    list_filter = ('semester', 'special')
+    search_fields=['prereq__prereq', 'prereq__this_or']
