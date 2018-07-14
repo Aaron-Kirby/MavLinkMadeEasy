d26ead2abbf6287a1152afe07cf8fcdf293dae2c
Lauren Koperski <laurenkoperski@gmail.com>
2018-07-14 16:36:51 -0500

    udpated TEMPLATES to recognize tempates

--- mavAgenda/mavAgenda/settings.py
+++ mavAgenda/mavAgenda/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'mavAgenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'landing/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
