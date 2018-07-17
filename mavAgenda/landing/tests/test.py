4dbc7a7e9fc22f2040f7c80bf01503a338332e0e
Kirby <Aaron_Kirby22@yahoo.com>
2018-07-17 16:59:53 -0500

    Test scripts for the submit buttons on each page. Currently the tests are successful and they link to the correct pages.

--- mavAgenda/landing/tests/test.py
+++ mavAgenda/landing/tests/test.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+
 class UITests(StaticLiveServerTestCase):
 
     @classmethod
@@ -17,3 +18,21 @@ class UITests(StaticLiveServerTestCase):
         selenium.get('http://127.0.0.1:8000/landing/createuser/')
         self.selenium.find_element_by_name("submit").click()
         assert 'schedule' in selenium.page_source
+
+    def test_selectcourses_form(self):
+        selenium = self.selenium
+        selenium.get('http://127.0.0.1:8000/landing/selectcourses/')
+        self.selenium.find_element_by_name("submit").click()
+        assert 'schedule' in selenium.page_source
+
+    def test_addclasses_form(self):
+        selenium = self.selenium
+        selenium.get('http://127.0.0.1:8000/landing/schedule/')
+        self.selenium.find_element_by_name("addclasses").click()
+        assert 'schedule' in selenium.page_source
+
+    def test_changemajor_form(self):
+        selenium = self.selenium
+        selenium.get('http://127.0.0.1:8000/landing/schedule/')
+        self.selenium.find_element_by_name("changemajor").click()
+        assert 'Landing Page' in selenium.page_source
