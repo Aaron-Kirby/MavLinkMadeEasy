1cbfd16cd8dbaa4ea659a0907a1fee0dee07f5ee
Lauren Koperski <laurenkoperski@gmail.com>
2018-07-17 12:21:19 -0500

    Continued UI tests- checkboxes, dropdowns, and text fields - oh my!

--- mavAgenda/landing/tests/test_UI.py
+++ mavAgenda/landing/tests/test_UI.py
@@ -1,5 +1,8 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 class UITests(StaticLiveServerTestCase):
 
@@ -15,5 +18,11 @@ class UITests(StaticLiveServerTestCase):
     def test_createuser_form(self):
         selenium = self.selenium
         selenium.get('http://127.0.0.1:8000/landing/createuser/')
+        self.selenium.find_element_by_name("emailbox").send_keys("test@test.com")
+        degreedd = Select(self.selenium.find_element_by_name('degreedd'))
+        degreedd.select_by_visible_text("bachelor's of science")
+        majordd = Select(self.selenium.find_element_by_name('majordd'))
+        majordd.select_by_visible_text("computer science")
+        self.selenium.find_element_by_name("agree").click()
         self.selenium.find_element_by_name("submit").click()
         assert 'schedule' in selenium.page_source
