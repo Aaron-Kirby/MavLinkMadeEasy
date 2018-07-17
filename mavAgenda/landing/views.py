80e20393eb630d91a2b6fa2567db0ded19283087
Lauren Koperski <laurenkoperski@gmail.com>
2018-07-17 15:57:27 -0500

    attempt at setup for passing user info createuser-->selectcourses

--- mavAgenda/landing/views.py
+++ mavAgenda/landing/views.py
@@ -4,20 +4,35 @@ from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 
-from .models import User, Course, Degree, Req, Prereq, Complete
+from .models import *
+from .forms import *
 
 def login(request):
     return render(request, 'landing/login.html')
 
 def selectcourses(request):
-    return render(request, 'landing/selectcourses.html')
+    if request.method == "POST":
+        form = UserCompletedForm(request.POST)
+        if form.is_valid():
+            courses = form.save(commit=False)
+            courses.save()
+            return HttpResponseRedirect(reverse('landing:schedule'))
+    else:
+        form = UserCompletedForm()
+    return render(request, 'landing/selectcourses.html', {'form': form})
 
 def schedule(request):
     return render(request, 'landing/schedule.html')
 
 def createuser(request):
-    return render(request, 'landing/createuser.html')
-
-
-
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            #return render(request, 'landing/selectcourses.html', {'form': form})
+            return HttpResponseRedirect(reverse('landing:selectcourses'))
+    else:
+        form = UserForm()
+    return render(request, 'landing/createuser.html', {'form': form})
 
