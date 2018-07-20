e1f8559160e287aa041a8121cfd5f80e7cd35ce4
kt-sin <katie.sindelar@gmail.com>
2018-07-20 00:09:47 +0000

    current forms via Lauren

--- mavAgenda/landing/forms.py
+++ mavAgenda/landing/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+from .models import *
 
-class LoginForm(forms.Form):
-    email = forms.CharField(label='email', max_length=100)
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'degree')
+
+
+class UserCompletedForm(forms.ModelForm):
+    class Meta:
+        model = Complete
+        fields = ('user', 'complete')
