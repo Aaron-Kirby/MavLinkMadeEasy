7bb4e7708e16fe9a3188246253b2a13f6e593e62
Lauren Koperski <laurenkoperski@gmail.com>
2018-07-24 13:38:16 -0500

    add test for getting degree

--- mavAgenda/landing/tests/test_logic.py
+++ mavAgenda/landing/tests/test_logic.py
@@ -11,7 +11,6 @@ class YourTestClass(TestCase):
 
     def tearDown(self):
         #Clean up run after every test method.
-        # need to remove the user...
         pass
 
     def test_get_user_by_email(self):
@@ -19,14 +18,14 @@ class YourTestClass(TestCase):
         d.save()
         u = User(email="sample@test.com", degree=d)
         u.save()
-        uID = u.id
         foundUser = getUserByEmail(u.email)
         self.assertTrue(u==foundUser)
 
-    #def test_get_degree(self):
-        #d = Degree(degree="Bachelor's of Science", major="CS")
-        #d.save()
-        #u = User(email="sample@test.com", degree=d)
-        #u.save()
-        #foundUser = getUserByEmail(u.email)
-        #self.assertTrue(u==foundUser)
+    def test_get_degree(self):
+        deg = Degree(degree="Bachelor's of Science", major="CS")
+        deg.save()
+        foundDegree = getDegree("Bachelor's of Science", "CS")
+        self.assertTrue(deg==foundDegree)
+
+    #def test_get_completed_by_user(self):
+        #self.assertTrue(False)
