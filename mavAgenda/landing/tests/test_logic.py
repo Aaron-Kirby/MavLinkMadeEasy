8183b9b1bc36129c66d662765c913f01ffa92fdc
Aaron Kirby <Aaron_Kirby22@yahoo.com>
2018-08-05 21:55:52 -0500

    Completed a test and have placeholders for others in test_logic

--- mavAgenda/landing/tests/test_logic.py
+++ mavAgenda/landing/tests/test_logic.py
@@ -27,5 +27,47 @@ class YourTestClass(TestCase):
         foundDegree = getDegree("Bachelor's of Science", "CS")
         self.assertTrue(deg==foundDegree)
 
-    #def test_get_completed_by_user(self):
-        #self.assertTrue(False)
+    #for user at inception
+    def test_get_completed_by_user(self):
+        d = Degree(degree="Bachelor's of Science", major="CS")
+        d.save()
+        u = User(email="completedByUser@test.com", degree=d)
+        u.save()
+        uID = u.pk
+        completedCourses = []
+        self.assertTrue(completedCourses == getCompletedByUser(uID))
+
+
+    #def test_get_courses_for_user(self):
+
+    #def test_remove_courses_taken(self):
+
+    #def test_check_prereq_met(self):
+
+    #def test_check_offered_semester(self):
+
+    #def test_check_course_valid(self):
+
+    #def test_get_semester_by_month_year(self):
+
+    #def test_generate_new_semester(self):
+
+    #def test_is_full(self):
+
+    #def test_create_schedule(self):
+
+    #def test_generate_checkbox_entities(self):
+
+    #def test_generate_major_id(self):
+
+    def test_email_found(self):
+        d = Degree(degree="Bachelor's of Science", major="CS")
+        d.save()
+        u = User(email="email@test.com", degree=d)
+        u.save()
+        foundEmail = emailFound(u.email)
+        self.assertTrue(foundEmail)
+
+    #def test_save_classes_to_user(self):
+
+    #def test_remove_user_completed_entries(self):
