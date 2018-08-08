241ffefedc3ac90f2f4363cdd069bf6ec5e15c37
Lauren Koperski <laurenkoperski@gmail.com>
2018-08-07 21:49:57 -0500

    get UI tests working again :)

--- mavAgenda/landing/tests/test_UI.py
+++ mavAgenda/landing/tests/test_UI.py
@@ -25,32 +25,29 @@ class UITests(StaticLiveServerTestCase):
 
     def test_createuser_form(self):
         selenium = self.selenium
-        wait = WebDriverWait(selenium, 10)
         selenium.get('http://127.0.0.1:8000/landing/createuser/')
-        email = self.selenium.find_element_by_name("emailbox")
-        email.send_keys("test@test.com")
-        #degreedd = Select(self.selenium.find_element_by_name('degreedd'))
-        #degreedd.select_by_visible_text("bachelor's of science")
-        #majordd = Select(self.selenium.find_element_by_name('majordd'))
-        #majordd.select_by_visible_text("computer science")
-        #self.selenium.find_element_by_name("agree").click()
-        #self.selenium.find_element_by_name("submit").click()
-        #assert 'checkBoxes' in selenium.page_source
-        assert True
+        self.selenium.find_element_by_name("e-email").send_keys("test@test.com")
+        Select(self.selenium.find_element_by_name('d-degree')).select_by_value("Bachelor of Science")
+        Select(self.selenium.find_element_by_name('d-major')).select_by_value("Computer Science")
+        self.selenium.find_element_by_name("submit").click()
+        self.assertIn("MavLink", selenium.title)
 
-    #def test_selectcourses_form(self):
-        #selenium = self.selenium
-        #selenium.get('http://127.0.0.1:8000/landing/selectcourses/1')
-        #self.selenium.find_element_by_name("submit").click()
-        #assert True
-        #assert 'schedule' in selenium.page_source
+    def test_selectcourses_form(self):
+        selenium = self.selenium
+        selenium.get('http://127.0.0.1:8000/landing/selectcourses/1')
+        self.selenium.find_element_by_name("submit").click()
+        self.assertIn("MavLink", selenium.title)
 
-    #def test_addclasses_form(self):
-        #selenium = self.selenium
-        #selenium.get('http://127.0.0.1:8000/landing/schedule/')
-        #self.selenium.find_element_by_name("addclasses").click()
-        #assert 'schedule' in selenium.page_source
+    def test_addclasses_form(self):
+        selenium = self.selenium
+        selenium.get('http://127.0.0.1:8000/landing/schedule/1')
+        self.selenium.find_element_by_name("updateCourses").click()
+        self.assertIn("MavLink", selenium.title)
 
+    # this UI test is for a link from schedule to have the user change major...
+    # right now, there isn't enough functionality to state the the user should be
+    # updated (as opposed to creating a new one) so this test doesn't make sense to
+    # write yet, although it would be helpful eventually :)
     #def test_changemajor_form(self):
         #selenium = self.selenium
         #selenium.get('http://127.0.0.1:8000/landing/schedule/')
